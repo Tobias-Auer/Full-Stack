bc56a3591713df57497b0b2c94d5c14dc6972e17
Tobias Auer <tobias.auer@aol.com>
2023-07-13 20:38:56 +0200

    Added some files

---
+++ minecraft_webserver/main.py
@@ -0,0 +1,40 @@
+import os
+import sqlite3
+import threading
+import time
+
+import dataBaseOperations
+import utils
+
+import logging
+import sys
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s', '%m-%d-%Y %H:%M:%S')
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler('logs.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stdout_handler)
+
+logger.info('Starting')
+def check_shutdown():
+    while True:
+        if dataBaseOperations.checkForKey("meta", "doAction", "shutdown"):
+            utils.doShutdownRoutine(logger)
+            break
+        time.sleep(5)
+
+def main():
+    thread = threading.Thread(target=check_shutdown)
+    thread.start()
+
+if __name__ == '__main__':
+    main()
